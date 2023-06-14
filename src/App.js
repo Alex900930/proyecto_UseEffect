0afac48fc4fc90900bccff4f9f4abb03c93bf0ed
Alexander Herrera <Alex>
2023-06-14 17:38:33 -0400

    actividad del useEffect finalizada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [nombre, setNombre] = useState("");
+  const [posicion, setPosicion] = useState(0);
+ 
+  const otro = () => {
+    const url = 'https://reqres.in/api/users?page=2';
+    let peticion = fetch(url);
+    peticion
+   .then(datos => datos.json())
+   .then((lectura) => {
+    setNombre(lectura.data[posicion].first_name);
+    setPosicion(posicion + 1)
+  })
+   .catch(() => console.log("Ocurrio un error"));
+  }
+
+ useEffect(() => {
+  
+ }, []) 
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Premiados del mes: </h1>
+      {nombre}
+      <button onClick={otro}>Otro nombre</button>
+    </>
   );
 }
 
